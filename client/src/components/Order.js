1fbf38f617b3dc27519ae7dbb7fa26ef755e8efb
kjsaha <70860766+kjsaha@users.noreply.github.com>
2021-06-23 22:46:25 -0500

    first commit

---
+++ client/src/components/Order.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import AppNavbar from "./AppNavbar";
+
+class Order extends Component {
+  render() {
+    return (
+      <div>
+        <AppNavbar />
+        <h1>Not yet implemented</h1>
+      </div>
+    );
+  }
+}
+
+export default Order;
